27aa10200b7f8e78c3f0ca442a8dd2dc44d4920f
Kedar.K <kedarkodgire.kk@gmail.com>
2020-10-09 23:57:49 +0530

    Day 10

---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+
+import Aux from '../../HOC/Auxilary'
+
+class BurgerBuilder extends Component{
+    render() {
+        return (
+            <Aux>
+                <div>
+                    Burger
+                </div>
+                <div>
+                    Burger Controls
+                </div>
+            </Aux>
+            
+        )
+    }
+}
+
