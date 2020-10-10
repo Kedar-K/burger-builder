90d0cc2d4599e47a93033e6e1f2e30692886b361
Kedar.K <kedarkodgire.kk@gmail.com>
2020-10-11 00:10:23 +0530

    Day 12

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -3,13 +3,19 @@ import classes from './Burger.module.css';
 import BurgerIngridient from '../Burger/BurgerIngridient/BurgerIngridient'
 
 const burger = (props) =>{
-    const transformedIngridients = Object.keys(props.ingridients)
+    let transformedIngridients = Object.keys(props.ingridients)
         .map(igKey => {
             return [...Array(props.ingridients[igKey])].map((_, i)=>{
                 return <BurgerIngridient key={igKey + i} type={igKey}/>
-            })
+            });
         })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
 
+    if (transformedIngridients.length === 0){
+        transformedIngridients = <p>please start adding ingridients</p>
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngridient type="bread-top" />
